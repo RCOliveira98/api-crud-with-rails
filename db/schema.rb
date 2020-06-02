b8937d4f7f7f7cdbf2a6f1cd70ebfd12d23c3be2
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-06-01 22:18:55 -0300

    Create resource Phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_011349) do
+ActiveRecord::Schema.define(version: 2020_06_02_011828) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.date "birthdate"
+    t.integer "kind_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["kind_id"], name: "index_contacts_on_kind_id"
+  end
 
   create_table "kinds", force: :cascade do |t|
     t.string "description"
@@ -18,4 +28,14 @@ ActiveRecord::Schema.define(version: 2020_06_02_011349) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "phones", force: :cascade do |t|
+    t.string "number"
+    t.integer "contact_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_phones_on_contact_id"
+  end
+
+  add_foreign_key "contacts", "kinds"
+  add_foreign_key "phones", "contacts"
 end
