ca15c974254b41a9059246fd2105f5341534a37a
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-06-09 15:31:27 -0300

    [serializers] update contact and create phone and address

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,15 +1,21 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id)}
+  end
   has_many :phones
   has_one :address
+
+  # add links
+  # link(:self) { contact_url(object.id) }
+  # link(:kind) { kind_url(object.kind.id)}
 
   def attributes(*args)
     contact = super(*args)
     # pt-BR ---> contact[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
     contact[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
-    contact
+    contact # return
   end
 
 end
