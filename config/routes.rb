6548c8da773a3c634c0c52be3b25dd9be9230e0d
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-06-24 15:46:11 -0300

    [config] maps routes for api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,45 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :phones
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+    resources :phones
 
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+  
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+  
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+  
+      resource :address, only: [:show,:update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  
+    resources :kinds
+  end
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+  api_version(:module => "V2", :path => {:value => "v2"}) do
+    resources :phones
 
-    resource :phone, only: [:update, :create, :destroy]
-    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
-
-    resource :address, only: [:show,:update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+  
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+  
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+  
+      resource :address, only: [:show,:update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  
+    resources :kinds
   end
   
-  resources :kinds
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
